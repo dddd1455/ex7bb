da6438550f1db57a6820f463e1237fb10b54dc18
thanathorn <patu@example.com>
2025-04-22 20:48:58 +0700

    Bug fix#2 – adding student id validation (id must be composed of numbers between 0 and 9)

--- validate.js
+++ validate.js
@@ -1,34 +1,32 @@
 function checkSID() {
-  let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
-    return true;
-  } else {
-    return false;
+	let sid = (document.getElementById("sid").value).trim();
+	const regex = /^[0-9]{10}$/;
+	return regex.test(sid);
   }
-}
-
-function checkCandiNo() {
-  let candi = (document.getElementById("candi").value).trim();
-  if (isNaN(candi) || candi < 1) {
-    return false;
-  } else {
-    return true;
+  
+  function checkCandiNo() {
+	let candi = (document.getElementById("candi").value).trim();
+	if (isNaN(candi) || candi < 1) {
+	  return false;
+	} else {
+	  return true;
+	}
   }
-}
-
-function validateForm(){
+  
+  function validateForm(){
 	if(!checkSID()){
-	  alert("Invalid value for Student ID!");
+	  alert("รหัสนักศึกษาต้องประกอบด้วยจำนวนเต็มระหว่าง 0 ถึง 9 เท่านั้น และต้องมีความยาว 10 หลัก");
 	  document.getElementById("sid").focus();
 	  return false;
-	}else{
-		if(!checkCandiNo()){
-		  alert("Invalid value for Candidate No!!");
-		  document.getElementById("candi").focus();
-		  return false;
-		}else{
-			alert("Your input data passes validation!!");
-			return true;
-		}
+	} else {
+	  if(!checkCandiNo()){
+		alert("กรุณากรอกหมายเลขผู้สมัครให้ถูกต้อง (ต้องเป็นตัวเลขตั้งแต่ 1 ขึ้นไป)");
+		document.getElementById("candi").focus();
+		return false;
+	  } else {
+		alert("ข้อมูลของคุณผ่านการตรวจสอบเรียบร้อยแล้ว");
+		return true;
+	  }
 	}
-}
+  }
+  
